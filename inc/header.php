76ac67c7a4d047875d758c3df8f4c93bdb74d897
Nichole Dwight <nicholedwight@gmail.com>
2014-03-26 17:05:37 +0000

    Background image and beginning of forms added

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cat-egorical | Welcome</title>
     <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <script src="js/vendor/modernizr.js"></script>
   </head>
-  <body>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('inc/header.php') ?>
+<body id="homepage">
+
+
+
+<form id="registration" method="POST" action="register.php">
+  <div>
+    <label for="name">Name*</label>
+    <input type="text" class="form-control" name="name" value="" id="name">
+  </div>
+
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
