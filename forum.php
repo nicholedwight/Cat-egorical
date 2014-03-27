8bb38eeb201e6436042d7d4de2a6dd4482b6b029
Nichole Dwight <nicholedwight@gmail.com>
2014-03-27 08:56:16 +0000

    Forum page now displays MySQL table data. Unstyled

--- forum.php
+++ forum.php
@@ -1,5 +1,24 @@
 <?php include("inc/header.php") ?>
+<?php
+$db = connectToDatabase();
 
+    $query = "SELECT * FROM `questions`";
+
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $row_count = $statement->rowCount();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    if ( $row_count > 0) {
+      $topic = $result['subject'];
+      echo $topic;
+      $qbody = $result['question'];
+      echo $qbody;
+    } else {
+      echo "You suck.";
+    }
+
+?>
 
 
 
