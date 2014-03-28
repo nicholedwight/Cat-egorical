2dcc36b12bde09c738eb134dea4aaa57d2c365c1
Nichole Dwight <nicholedwight@gmail.com>
2014-03-28 07:10:41 +0000

    Added topic page styles

--- topic.php
+++ topic.php
@@ -8,13 +8,17 @@ if ($_GET) {
     $statement = $db->prepare($query);
     $statement->execute();
     $result = $statement->fetch(PDO::FETCH_ASSOC); ?>
-    <div class="topic">
+    <body id="topic">
+    <div class="profile-info">
       <h1>
       <?php echo $result['subject']; ?>
       </h1>
       <p>
       <?php echo $result['question']; ?>
       <h3>Asked by <a href="profile.php?id=<?php echo $result['userid']; ?>"><span class="underline"><?php echo getUsernameFromId($result['userid']); ?></span></a></h3>
+      </p>
+      <p>
+        Asked on <?php echo $result['created_at']; ?>
       </p>
     <?php if (getUsernameFromId($result['userid']) == $_SESSION['username']) { ?>
         <a href="update.php?id=<?php echo $result['id']; ?>" class="small round button">Edit</a>
