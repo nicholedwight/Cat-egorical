10b7afe5c49775870557cad02f703102eeeb8b76
Nichole Dwight <nicholedwight@gmail.com>
2014-04-03 11:56:48 +0100

    Delete account function works!

--- delete.php
+++ delete.php
@@ -2,10 +2,26 @@
 use forxer\Gravatar\Gravatar;
 include("inc/header.php"); ?>
 <body id="profile">
+<?php
+if ($_GET):
+    $db = connectToDatabase();
+    $user = $_GET['id'];
+    $query = "DELETE FROM `users` WHERE `id` = $user";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC); ?>
+
+<div class="response">
+<?php  if ($statement->errorCode() == 0) {
+  echo "Your account has successfully been deleted!";
+  } else {
+  $errors = $statement->errorInfo();
+  echo "Sorry, something messed up! Try again!";
+} ?>
+</div>
 
 
 
-
-
-
-<?php include("inc/footer.php") ?>
+<?php
+endif;
+include("inc/footer.php") ?>
