ece67d173db3eedfbd7f6f90ee7b63b5e1fa3330
Nichole Dwight <nicholedwight@gmail.com>
2014-04-06 15:16:41 +0100

    Categories working

--- submission.php
+++ submission.php
@@ -20,14 +20,36 @@ if ($_POST) {
   $statement = $db->prepare($query);
   $statement->execute();
 
-  if ($statement->errorCode() == 0) {
-    echo "Thanks! Your questions was recieved successfully!";
+  $questionid = $db->lastInsertId();
+  $catquery = "INSERT INTO `questions_have_categories` (`category_id`, `question_id`)
+               VALUES";
+  $i = 0;
+  $length = count($catquery);
+  foreach($_POST['category'] as $catid){
+    if ($length == 0) {
+                      $catquery .= "('" . $catid . "',
+                                   '" . $questionid . "'),";
+        } else if ($i == $length -1) {
+          $catquery .= "('" . $catid . "',
+                       '" . $questionid . "')";
+        } $i++;
+    }
+  $catstatement = $db->prepare($catquery);
+  $catstatement->execute();
+
+  if ($catstatement->errorCode() == 0) {
+    echo "Yay!";
   } else {
-    $errors = $statement->errorInfo();
+    $errors = $catstatement->errorInfo();
     echo($errors[2]);
   }
 }
 
+  $getcatquery = "SELECT * FROM `categories`";
+
+  $getcatstatement = $db->prepare($getcatquery);
+  $getcatstatement->execute();
+  $getcatresult = $getcatstatement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <body id="subpage">
@@ -36,6 +58,14 @@ if ($_POST) {
   <form id="questions" method="POST">
     <label for="subject" required>Subject</label>
     <input type="text" name="subject" value="" id="subject" placeholder="Subject" required>
+
+    <p>Category (Select all that apply)</p>
+    <?php foreach ($getcatresult as $category): ?>
+    <div class="catboxes">
+      <label for="<?php echo $category['name']; ?>"><?php echo $category['name']; ?></label>
+      <input type="checkbox" name="category[]" value="<?php echo $category['id']; ?>">
+    </div>
+  <?php endforeach; ?>
 
     <label for="question" required>Question</label>
     <textarea name="question" id="question" rows="6" placeholder="Enter your Cat-y questions here!" required></textarea>
